0ba0e3d1f21f5f9e6f86d5923be2a028263e0e28
Abhay Goswami <abhaygoswami676@gmail.com>
2021-07-14 15:41:49 +0530

    feat: add response body and messages

---
+++ src/api/utils/Messages.js
@@ -0,0 +1,37 @@
+// ----------------------------
+//! User messages
+// -----------------------------
+export const USER_CREATED = 'user created successfully';
+export const USER_CREATED_ERROR = 'error occur, while creating a user';
+// ----------------------------
+//! Questions messages
+// -----------------------------
+export const QUESTIONS = 'all questions fetched successfully'
+export const NEW_QUESTION = 'question created successfully';
+export const NEW_QUESTION_ERROR = 'error occur, while creating a question';
+export const QUESTION_FETCHED = 'question fetched successfully'
+// ----------------------------
+//! Answer messages
+// -----------------------------
+export const NEW_ANSWER = 'answer created successfully';
+export const NEW_ANSWER_ERROR = 'error occur, while creating a answer';
+export const ANSWER_ACCEPTED = 'answer accepted successfully';
+export const ANSWER_ACCEPTED_ERROR = 'error occur, while accepting a answer';
+// ----------------------------
+//! Transaction messages
+// -----------------------------
+export const LIKE = 'transaction recorded successfully';
+export const LIKE_ERROR = 'error occur, while recording the transaction';
+export const NEW_REPLY = 'reply created successfully';
+export const NEW_REPLY_ERROR = 'error occur, while creating a reply';
+// ----------------------------
+//! Channel messages
+// -----------------------------
+export const NEW_CHANNEL = 'channel created successfully';
+export const NEW_CHANNEL_ERROR = 'error occur, while creating a channel';
+export const CHANNEL_DETAILS = 'channel fetch successfully'
+// ----------------------------
+//! Upload messages
+// -----------------------------
+export const UPLOAD = 'image uploaded successfully'
+export const UPLOAD_ERROR='error occur, while uploading'
