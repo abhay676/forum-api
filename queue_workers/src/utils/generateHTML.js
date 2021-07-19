b7e18c2ed2f8fac342561d414f90ca8366ebee42
Abhay Goswami <abhaygoswami676@gmail.com>
2021-07-19 19:39:39 +0530

    feat: OTP verification mail

--- queue_workers/src/utils/generateHTML.js
+++ queue_workers/src/utils/generateHTML.js
@@ -1,14 +1,22 @@
 import { emailTemplate } from '../nodemailer.js';
 export const generateHTML = async (data) => {
+  let HTML = '';
   switch (data.type) {
     case 'WELCOME':
       // call signUp template
-      const HTML = await emailTemplate(`welcome.ejs`, {
+      HTML = await emailTemplate(`welcome.ejs`, {
         name: data.name,
       });
       return HTML;
-
+    case 'LOGIN':
+      HTML = await emailTemplate('otp.ejs', {
+        OTP: data.OTP,
+        IP: data.IP,
+        userAgent: data.userAgent,
+        userName: data.userName,
+      });
+      return HTML;
     default:
-      break;
+      return HTML;
   }
 };
